9659560b9e688b77241dff1ce5d754243beb0ca1
Jalil Sedna <36819130+jalilsedna@users.noreply.github.com>
2026-06-21 09:16:26 +0330

    Enable host and set allowed hosts in Vite config
    Enable host and set allowed hosts in Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,8 +12,10 @@ export default defineConfig({
     }
   },
   server: {
+    host: true,
     port: 3000,
     open: true,
+    allowedHosts: ['.railway.app'],
     proxy: {
       '/api': {
         target: 'http://localhost:5001',
